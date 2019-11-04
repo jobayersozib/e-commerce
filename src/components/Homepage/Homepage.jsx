73e883cd32fe00772f926a4f4cab6453336d6212
jobayermahamud <jobayermahamudkamalcse@gmail.com>
2019-11-04 19:08:59 +0600

    few change in home page

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import './Homepage.css.scss'
+import React from 'react';
+import './Homepage.css.scss';
 
-const Homepage =()=>{
+const Homepage = () => {
   return (
     <div className="homepage">
       <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
-              <h1 className="title">Title One</h1>
-              <h3 className="sub-title">Sub-Title One</h3> 
+            <h1 className="title">Title One</h1>
+            <span className="subtitle">Sub-Title One</span>
           </div>
           <div className="content">
-              <h1 className="title">Title Two</h1>
-              <h3 className="sub-title">Sub-Title Two</h3> 
+            <h1 className="title">Title Two</h1>
+            <span className="subtitle">Sub-Title One</span>
           </div>
           <div className="content">
-              <h1 className="title">Title Three</h1>
-              <h3 className="sub-title">Sub-Title Three</h3> 
+            <h1 className="title">Title Three</h1>
+            <span className="subtitle">Sub-Title One</span>
           </div>
           <div className="content">
-              <h1 className="title">Title Four</h1>
-              <h3 className="sub-title">Sub-Title Four</h3> 
+            <h1 className="title">Title Four</h1>
+            <span className="subtitle">Sub-Title One</span>
           </div>
           <div className="content">
-              <h1 className="title">Title Five</h1>
-              <h3 className="sub-title">Sub-Title Five</h3> 
+            <h1 className="title">Title Five</h1>
+            <span className="subtitle">Sub-Title One</span>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export {Homepage}
+export { Homepage };
