3011626e31585d19cd6247083aec508c0f970cde
jobayermahamud <jobayermahamudkamalcse@gmail.com>
2019-11-05 18:01:13 +0600

    Refactor menu item and add router to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Homepage } from './components/Homepage/Homepage';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+const Hats = () => {
+  return <div>Hats page</div>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Homepage />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hats} />
       </div>
     );
   }
--- src/components/Directory/Directory.component.jsx
+++ src/components/Directory/Directory.component.jsx
@@ -10,28 +10,33 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          linkUrl: 'hats',
           id: 1
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          linkUrl: 'jackets',
           id: 2
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          linkUrl: 'sneakers',
           id: 3
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
+          linkUrl: 'womens',
           id: 4
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
+          linkUrl: 'mens',
           id: 5
         }
       ]
@@ -47,6 +52,7 @@ class Directory extends React.Component {
               key={data.id}
               imageUrl={data.imageUrl}
               title={data.title}
+              linkUrl={data.linkUrl}
               size={data.size}
             />
           );
