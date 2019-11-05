0c6704affbef4d450f3a8999ff47c45980d75bb2
jobayermahamud <jobayermahamudkamalcse@gmail.com>
2019-11-05 13:02:02 +0600

    Add directory component animation and menu item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Homepage } from './components/Homepage/Homepage'
+import { Homepage } from './components/Homepage/Homepage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Homepage/>
+        <Homepage />
       </div>
     );
   }
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
+import { Directory } from '../Directory/Directory.component';
 import './Homepage.css.scss';
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Title One</h1>
-            <span className="subtitle">Sub-Title One</span>
-          </div>
-          <div className="content">
-            <h1 className="title">Title Two</h1>
-            <span className="subtitle">Sub-Title One</span>
-          </div>
-          <div className="content">
-            <h1 className="title">Title Three</h1>
-            <span className="subtitle">Sub-Title One</span>
-          </div>
-          <div className="content">
-            <h1 className="title">Title Four</h1>
-            <span className="subtitle">Sub-Title One</span>
-          </div>
-          <div className="content">
-            <h1 className="title">Title Five</h1>
-            <span className="subtitle">Sub-Title One</span>
-          </div>
-        </div>
-      </div>
+      <h1>Welcome to my Homepage</h1>
+      <Directory />
     </div>
   );
 };
