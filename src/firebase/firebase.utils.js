c2a82107257221576fad9ef1e71dd5a80b717783
jobayermahamud <jobayermahamudkamalcse@gmail.com>
2019-12-10 13:53:53 +0600

    Make some changes

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const config = {
-  apiKey: 'AIzaSyDAmp9vuR-KC8IyAuEoN0ZSqEpUDRBvLNo',
-  authDomain: 'crwn-db-3b7c4.firebaseapp.com',
-  databaseURL: 'https://crwn-db-3b7c4.firebaseio.com',
-  projectId: 'crwn-db-3b7c4',
-  storageBucket: 'crwn-db-3b7c4.appspot.com',
-  messagingSenderId: '58727150111',
-  appId: '1:58727150111:web:adefd03ea990d8b391db2a',
-  measurementId: 'G-HX076FBL84'
+  apiKey: "AIzaSyDAmp9vuR-KC8IyAuEoN0ZSqEpUDRBvLNo",
+  authDomain: "crwn-db-3b7c4.firebaseapp.com",
+  databaseURL: "https://crwn-db-3b7c4.firebaseio.com",
+  projectId: "crwn-db-3b7c4",
+  storageBucket: "crwn-db-3b7c4.appspot.com",
+  messagingSenderId: "58727150111",
+  appId: "1:58727150111:web:adefd03ea990d8b391db2a",
+  measurementId: "G-HX076FBL84"
 };
 
 firebase.initializeApp(config);
@@ -26,6 +26,7 @@ export const createUserProfileDocument = (userOnfo, callback) => {
     //console.log(data)
     if (!data.exists) {
       userRef.set({
+        uid: userOnfo.uid,
         displayName: userOnfo.displayName,
         email: userOnfo.email,
         createdAt: new Date()
@@ -36,7 +37,7 @@ export const createUserProfileDocument = (userOnfo, callback) => {
   callback(userRef);
 };
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
